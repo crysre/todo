9972e5a54f05d051244d18d0608def94457219b8
Crysre <tamosrk@gmail.com>
2025-09-12 20:03:41 +0530

    Added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
+import Create from "./Components/create"
+import Navbar from "./Components/Navbar"
 
 
 
 function App() {
 
 
-  return <>
-  Hi there
-  </>
+  return <div className="flex flex-col" >
+  <Navbar/>
+  <Create/>
+  </div>
 }
 
 export default App
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return <div className=" rounded h-16 w-screen px-5 flex items-center text-white justify-between sticky top-0 bg-black">
+        <p className=" text-2xl" >Notes</p>
+        <p className="text-xl" >Login</p>
+    </div>
+}
+
+export default Navbar
