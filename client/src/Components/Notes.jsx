c7b4565e27d2f77c267043424fe00a66238e8903
Crysre <tamosrk@gmail.com>
2025-09-15 19:13:19 +0530

    added fetch notes

--- client/src/Components/Notes.jsx
+++ client/src/Components/Notes.jsx
@@ -1,23 +1,37 @@
+import { useEffect } from "react";
+import { getNotes } from "../api/notes";
+import { useState } from "react";
+
 function Notes(){
+
+    const [notes, setNotes] = useState([]);
+
+    useEffect(()=>{
+        async function fetchNotes(){
+            const response = await getNotes();
+            setNotes(response.data.notes)
+
+        }
+
+        fetchNotes()
+
+    }, [])
+
+    console.log(notes);
+    
+
+
     return <div className="ml-5" >
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
-         <Note/>
+         {notes.map((note, index)=>{
+            return <Note key={index} title={note.title} body={note.body} />
+         })}
     </div>
 }
 
-function Note(){
-    return <div className=" shadow-2xl float-left mx-5 my-5 rounded-xl p-2  h-16 w-60 bg-gray-200" >
-        <h1 className="text-black" >adsufbh1ewiufb</h1>
-        <p className="text-black" >;wjdn;oend</p>
+function Note({title, body}){
+    return <div className=" shadow-2xl float-left mx-5 my-5 rounded-xl p-2 w-60 bg-gray-200" >
+        <h1 className="text-black" >{title}</h1>
+        <p className="text-black" >{body}</p>
     </div>
 }
 
