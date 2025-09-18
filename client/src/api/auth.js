0bfc00e00ec0a4a56acc6409d664618fc808be1d
Crysre <tamosrk@gmail.com>
2025-09-18 16:08:41 +0530

    added error handling

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,16 +2,28 @@ import axios from "./axios" //importing custom instance
 
 
 async function handleSignup(data){
-    const response = await axios.post("http://localhost:3000/signup", data)
-    console.log(response);
+    try{
+        const response = await axios.post("http://localhost:3000/signup", data)
+        console.log(response);
+    }catch(e){
+        console.log("Signup failed ", e);
+        
+    }
+    
     
 }
 
 
 async function handleLogin(data){
+    try{
+        
     const response = await axios.post("http://localhost:3000/login", data)
     console.log(response);
     localStorage.setItem("token",response.data.token);
+    }catch(e){
+        console.log("Login failed ", e);
+        
+    }
 }
 
 
--- client/src/api/notes.js
+++ client/src/api/notes.js
@@ -7,18 +7,33 @@ async function submitNote(note){
 }
 
 async function getNotes(){
+    try{
+        
     const response = await axios.get("/notes")
     return response
+    }catch(e){
+        console.log("Fetching notes failed ", e)
+    }
 }
 
 async function editOrDeleteNotes(name, id, editedNote){
-    if (name=="edit"){
+    if (name==="edit"){
+
+        try{
+            
         const response = await axios.patch(`/notes/${id}`, editedNote);
         return response
+        }catch(e){
+            console.log("Patching notes failed ", e)
+        }
         
-    }else if(name=="delete"){
+    }else if(name==="delete"){
+        try{
         const response = await axios.delete(`/notes/${id}`)
         return response
+        }catch(e){
+            console.log("Deleting notes failed ", e)
+        }
     }
 }
 
